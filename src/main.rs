7374445bcd9a4e3b4efc82535d40c1abd8296d6e
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-05-13 00:49:06 +0800

    added to create non template cpp file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::fs;
 use std::io::Write;
+use std::path::Path;
 
 fn main() {
 
@@ -12,10 +13,20 @@ fn main() {
         .read_line(&mut file_path)
         .expect("Failed to Read Line");
     
-    let file_path = file_path.trim();
+    let file_path = Path::new(file_path.trim());
 
     let content = fs::read_to_string(file_path)
         .expect("Failed To Read File");
 
-    println!("{content}");
+    if !content.contains("template"){
+        let new_cpp_name_path = file_path.file_name().unwrap().to_str().unwrap();
+        let new_cpp_name_list: Vec<_> = new_cpp_name_path.split('.').collect();
+        let new_cpp_name = new_cpp_name_list[0];
+        let new_cpp_path = file_path.parent().unwrap().to_str().unwrap();
+
+        let new_cpp = new_cpp_path.to_owned() + "/" + new_cpp_name + ".cpp";
+        let mut new_cpp_file = fs::File::create(new_cpp);
+    }
+
+    // println!("{content}");
 }
