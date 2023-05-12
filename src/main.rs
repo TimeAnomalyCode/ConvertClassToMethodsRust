a8fa3599b2646d1553e4bec5b1d083f0993237b2
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-05-12 16:43:31 +0800

    add read to output file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::fs;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut file_path = String::new();
+
+    print!("Enter filePath: ");
+    let _ = io::stdout().flush();
+    io::stdin()
+        .read_line(&mut file_path)
+        .expect("Failed to Read Line");
+    
+    let file_path = file_path.trim();
+
+    let content = fs::read_to_string(file_path)
+        .expect("Failed To Read File");
+
+    println!("{content}");
 }
