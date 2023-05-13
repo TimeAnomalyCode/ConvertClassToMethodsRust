23bed021398e06bf41a26e513dd816a1abfb0b4c
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-05-13 13:42:34 +0800

    add func for publicmethods and classvariables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::OpenOptions;
 use std::io;
 use std::fs;
 use std::io::Write;
@@ -27,55 +28,57 @@ fn main() {
         let mut new_cpp_file = fs::File::create(new_cpp)
             .expect("Failed to create file");
 
-        let mut class_name = String::new();
+        // let mut class_name = String::new();
 
-        let mut is_public_method = false;
-        let mut public_methods: Vec<String> = vec![];
+        // let mut is_public_method = false;
+        // let mut public_methods: Vec<String> = vec![];
         
-        let mut is_class_variable = false;
-        let mut class_variables: Vec<String> = vec![];
+        // let mut is_class_variable = false;
+        // let mut class_variables: Vec<String> = vec![];
 
-        for line in content.lines(){
-            let new_line = line.trim_start();
-            if new_line.starts_with("//") || new_line.is_empty() || new_line.contains("#") || new_line.contains("{") || new_line.contains("}") || new_line.contains("virtual"){
-                continue;
-            }
+        // for line in content.lines(){
+        //     let new_line = line.trim_start();
+        //     if new_line.starts_with("//") || new_line.is_empty() || new_line.contains("#") || new_line.contains("{") || new_line.contains("}") || new_line.contains("virtual"){
+        //         continue;
+        //     }
 
-            if new_line.contains("class"){
-                class_name = new_line.replace("class", "");
-            }
+        //     if new_line.contains("class"){
+        //         class_name = new_line.replace("class", "");
+        //     }
 
-            if new_line.starts_with("public:"){
-                is_public_method = true;
-                continue;
-            }
+        //     if new_line.starts_with("public:"){
+        //         is_public_method = true;
+        //         continue;
+        //     }
 
-            if is_public_method {
-                if new_line.contains("private:") || new_line.contains("protected:") || new_line.contains("}"){
-                    is_public_method = false;
-                }
-                else {
-                    public_methods.push(new_line.to_string());
-                }
-            }
+        //     if is_public_method {
+        //         if new_line.contains("private:") || new_line.contains("protected:") || new_line.contains("}"){
+        //             is_public_method = false;
+        //         }
+        //         else {
+        //             public_methods.push(new_line.to_string());
+        //         }
+        //     }
 
-            if new_line.contains("protected:") || new_line.contains("private:"){
-                is_class_variable = true;
-                continue;
-            }
+        //     if new_line.contains("protected:") || new_line.contains("private:"){
+        //         is_class_variable = true;
+        //         continue;
+        //     }
 
-            if is_class_variable {
-                if new_line.contains("public:") || new_line.contains("}"){
-                    is_class_variable = false;
-                }
-                else {
-                    class_variables.push(new_line.to_string());
-                }
-            }
-            // println!("{}", new_line);
-        }
+        //     if is_class_variable {
+        //         if new_line.contains("public:") || new_line.contains("}"){
+        //             is_class_variable = false;
+        //         }
+        //         else {
+        //             class_variables.push(new_line.to_string());
+        //         }
+        //     }
+        //     // println!("{}", new_line);
+        // }
 
-        class_name = class_name.trim().to_owned();
+        // class_name = class_name.trim().to_owned();
+
+        let (public_methods, class_variables, class_name) = get_public_methods_and_class_variables_and_className(content);
 
         let mut template = format!(
             "#include <iostream>\n#include \"{file_name}\"\n\nusing namespace std;\n\n")
@@ -104,6 +107,66 @@ fn main() {
 
         new_cpp_file.write(template.as_bytes()).expect("Write Failed");
     }
+    else {
+        let mut new_cpp = OpenOptions::new()
+            .append(true)
+            .open(file_path)
+            .expect("Failed to Open File");
 
+        let (public_methods, class_variables, class_name) = get_public_methods_and_class_variables_and_className(content);
+
+    }
     // println!("{content}");
+}
+
+fn get_public_methods_and_class_variables_and_className(content: String) -> (Vec<String>, Vec<String>, String) {
+    let mut class_name = String::new();
+
+    let mut is_public_method = false;
+    let mut public_methods: Vec<String> = vec![];
+    
+    let mut is_class_variable = false;
+    let mut class_variables: Vec<String> = vec![];
+
+    for line in content.lines(){
+        let new_line = line.trim_start();
+        if new_line.starts_with("//") || new_line.is_empty() || new_line.contains("#") || new_line.contains("{") || new_line.contains("}") || new_line.contains("virtual"){
+            continue;
+        }
+
+        if new_line.contains("class"){
+            class_name = new_line.replace("class", "").trim().to_owned();
+        }
+
+        if new_line.starts_with("public:"){
+            is_public_method = true;
+            continue;
+        }
+
+        if is_public_method {
+            if new_line.contains("private:") || new_line.contains("protected:") || new_line.contains("}"){
+                is_public_method = false;
+            }
+            else {
+                public_methods.push(new_line.to_string());
+            }
+        }
+
+        if new_line.contains("protected:") || new_line.contains("private:"){
+            is_class_variable = true;
+            continue;
+        }
+
+        if is_class_variable {
+            if new_line.contains("public:") || new_line.contains("}"){
+                is_class_variable = false;
+            }
+            else {
+                class_variables.push(new_line.to_string());
+            }
+        }
+        // println!("{}", new_line);
+    }
+
+    return (public_methods, class_variables, class_name);
 }
